c4b368abef90a7ce9d40f50301609709b311e929
Pedro Fernandes Steimbruch <pedrofsteimbruch@gmail.com>
2015-10-13 15:48:01 -0300

    fixing PagesController

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -17,6 +17,7 @@ namespace App\Controller;
 use Cake\Core\Configure;
 use Cake\Network\Exception\NotFoundException;
 use Cake\View\Exception\MissingTemplateException;
+use Cake\Event\Event;
 
 /**
  * Static content controller

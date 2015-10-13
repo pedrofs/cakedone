7eba675b3d5bff2c35d5f36ab806aa6229d90551
Pedro Fernandes Steimbruch <pedrofsteimbruch@gmail.com>
2015-10-13 15:46:59 -0300

    adding app.js

---
+++ webroot/js/app.js
@@ -0,0 +1,42 @@
+(function () {
+	'use strict';
+
+	var cakedone = angular.module('cakedone', [
+		'ngRoute',
+		'ngAnimate',
+		'ui.router'
+	]);
+
+	cakedone.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+		$urlRouterProvider.otherwise('/');
+
+		$stateProvider
+			.state('public', {template: '<public></public>', abstract: true})
+			.state('public.login', {url: '/', template: '<login></login>'})
+			.state('public.register', {url: '/register', template: '<register></register>'});
+	}]);
+})();
+
+(function () {
+	'use strict';
+
+	angular.module('cakedone')
+		.directive('public', [function () {
+			return {
+				restrict: "E",
+				templateUrl: '/views/public/public.html',
+				controller: ['$scope', function ($scope) {
+					console.log('public state');
+				}]
+			};
+		}])
+		.directive('login', [function () {
+			return {
+				restrict: "E",
+				templateUrl: '/views/public/login.html',
+				controller: ['$scope', function ($scope) {
+					console.log('login state');
+				}]
+			};
+		}]);
+})();

262cc02aafa2a12be7d57475aa67a7e1bf533a13
Pedro Fernandes Steimbruch <pedrofsteimbruch@gmail.com>
2015-10-13 23:42:53 -0300

    adding common angular module

---
+++ webroot/js/common.js
@@ -0,0 +1,62 @@
+(function () {
+	'use strict';
+
+	angular.module('common', [])
+		.config(['$provide', '$httpProvider', function ($provide, $httpProvider) {
+			$provide.factory('formErrorsInterceptor', ['$q', function ($q) {
+				return {
+					responseError: function (rejection) {
+						var data = rejection.data;
+
+						if (data.errors) {
+							angular.forEach(data.errors, function (errors, field) {
+								var uierror = '';
+								angular.forEach(errors, function (error, errorName) {
+									uierror += uierror + error + ". ";
+								});
+
+								var parsleyField = $('#' + field).parsley();
+           			window.ParsleyUI.addError(parsleyField, 'error' + field, uierror);
+							})
+						}
+
+						return $q.reject(rejection);
+					}
+				};
+			}]);
+
+			$httpProvider.interceptors.push('formErrorsInterceptor');
+		}])
+		.factory('api', ['$http', function ($http) {
+			var m = {
+				register: register,
+				todos: todos
+			};
+
+			function todos(page) {
+				return $http.get(addPageToUrl('/todos.json', page))
+					.then(function (response) {
+						return response.data;
+					});
+			};
+
+			function register(user) {
+				return $http.post('/users/add.json', {
+					name: user.name,
+					email: user.email,
+					password: user.password,
+					password_confirmation: user.password_confirmation
+				});
+			};
+
+			function addPageToUrl(url, page) {
+				if (page) {
+					return url + '?page=' + page;
+				}
+
+				return url;
+			};
+
+			return m;
+		}]);
+})();
